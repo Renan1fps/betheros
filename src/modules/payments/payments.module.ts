2dd59ad2105e55ec991961931721b8cf0f80a59b
Renan1fps <renan.sergipe68@gmail.com>
2026-03-29 14:04:07 -0300

    refactor: move recurring payment to use case

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -3,11 +3,14 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { PaymentOrmEntity } from './infrastructure/persistence/entities/payment.orm-entity';
 import { PaymentRepository } from './infrastructure/persistence/repositories/payment.repository.impl';
 import { PAYMENT_REPOSITORY } from './domain/repositories/payment.repository';
+import { CreateRecurringPaymentUseCase } from "@modules/payments/application/use-cases/create-recurring-payment.use-case";
+import { SubscriptionsModule } from "@modules/subscriptions/subscriptions.module";
 
 @Module({
-  imports: [MikroOrmModule.forFeature([PaymentOrmEntity])],
+  imports: [MikroOrmModule.forFeature([PaymentOrmEntity]), SubscriptionsModule],
   providers: [
-    { provide: PAYMENT_REPOSITORY, useClass: PaymentRepository },
+    { provide: PAYMENT_REPOSITORY, useClass: PaymentRepository } ,
+    CreateRecurringPaymentUseCase
   ],
   exports: [PAYMENT_REPOSITORY],
 })
